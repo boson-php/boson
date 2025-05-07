87e0d7a55a9890a702e5f87d0f8c21e3914a6103
Kirill Nesmeyanov <nesk@xakep.ru>
2025-05-08 02:04:41 +0300

    Improve event listener provider

--- src/Dispatcher/EventListenerProvider.php
+++ src/Dispatcher/EventListenerProvider.php
@@ -12,36 +12,6 @@ use Boson\Dispatcher\Subscription\CancellableSubscriptionInterface;
  */
 trait EventListenerProvider
 {
-    /**
-     * Provides a simplified interface for subscribing to events.
-     *
-     * Option to register listener without a name:
-     * ```
-     * $ctx->on(function(ExampleEvent $e): void {
-     *     var_dump($e);
-     * });
-     * ```
-     *
-     * Option to register listener with name:
-     * ```
-     * $ctx->on(ExampleEvent::class, function(ExampleEvent $e): void {
-     *     var_dump($e);
-     * });
-     *
-     * // or
-     *
-     * $ctx->on(ExampleEvent::class, function(): void {
-     *     var_dump('ExampleEvent fired!');
-     * });
-     * ```
-     *
-     * @template TArgEvent of object
-     *
-     * @param class-string<TArgEvent>|\Closure(TArgEvent):void $eventOrListener
-     * @param \Closure(TArgEvent):void|null $listener
-     *
-     * @return CancellableSubscriptionInterface<TArgEvent>
-     */
     public function on(\Closure|string $eventOrListener, ?\Closure $listener = null): CancellableSubscriptionInterface
     {
         if ($eventOrListener instanceof \Closure) {
--- src/Dispatcher/EventListenerProviderInterface.php
+++ src/Dispatcher/EventListenerProviderInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Boson\Dispatcher;
 
+use Boson\Dispatcher\Subscription\CancellableSubscriptionInterface;
+
 interface EventListenerProviderInterface
 {
     /**
@@ -11,4 +13,36 @@ interface EventListenerProviderInterface
      * and intention subscriptions.
      */
     public EventListenerInterface $events { get; }
+
+    /**
+     * Provides a simplified interface for subscribing to events.
+     *
+     * Option to register listener without a name:
+     * ```
+     * $ctx->on(function(ExampleEvent $e): void {
+     *     var_dump($e);
+     * });
+     * ```
+     *
+     * Option to register listener with name:
+     * ```
+     * $ctx->on(ExampleEvent::class, function(ExampleEvent $e): void {
+     *     var_dump($e);
+     * });
+     *
+     * // or
+     *
+     * $ctx->on(ExampleEvent::class, function(): void {
+     *     var_dump('ExampleEvent fired!');
+     * });
+     * ```
+     *
+     * @template TArgEvent of object
+     *
+     * @param class-string<TArgEvent>|\Closure(TArgEvent):void $eventOrListener
+     * @param \Closure(TArgEvent):void|null $listener
+     *
+     * @return CancellableSubscriptionInterface<TArgEvent>
+     */
+    public function on(\Closure|string $eventOrListener, ?\Closure $listener = null): CancellableSubscriptionInterface;
 }
