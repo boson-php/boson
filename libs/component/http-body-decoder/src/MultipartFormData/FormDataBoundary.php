bff929ffa51b383c76bfcd8a613b4e3caa24f3fd
Kirill Nesmeyanov <nesk@xakep.ru>
2025-05-25 20:32:15 +0300

    Allow whitespaces in boundary and add multipart tests

--- libs/component/http-body-decoder/src/MultipartFormData/FormDataBoundary.php
+++ libs/component/http-body-decoder/src/MultipartFormData/FormDataBoundary.php
@@ -4,20 +4,40 @@ declare(strict_types=1);
 
 namespace Boson\Component\Http\Body\MultipartFormData;
 
+/**
+ * DTO that represents a boundary in `multipart/xxxx` requests.
+ *
+ * If the boundary value is 'WebKitFormBoundary7MA4YWxkTrZu0gW', then:
+ *  - `$segment` will be '--WebKitFormBoundary7MA4YWxkTrZu0gW'
+ *  - `$end` will be '--WebKitFormBoundary7MA4YWxkTrZu0gW--'
+ *
+ * @link https://www.rfc-editor.org/rfc/rfc2046.html#section-5.1.1
+ *
+ * @internal this is an internal library class, please do not use it in your code
+ * @psalm-internal Boson\Component\Http\Body
+ */
 final readonly class FormDataBoundary
 {
     /**
+     * The boundary segment used to separate parts in the multipart form data.
+     * This is the boundary value prefixed with `--`.
+     *
      * @var non-empty-string
      */
     public string $segment;
 
     /**
+     * The final boundary marking the end of the multipart form data.
+     * This is the boundary value prefixed and suffixed with `--`.
+     *
      * @var non-empty-string
      */
     public string $end;
 
     public function __construct(
         /**
+         * The raw boundary value from the `content-type` header
+         *
          * @var non-empty-string
          */
         public string $value,
--- libs/component/http-body-decoder/src/MultipartFormData/FormDataBoundaryFactory.php
+++ libs/component/http-body-decoder/src/MultipartFormData/FormDataBoundaryFactory.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace Boson\Component\Http\Body\MultipartFormData;
 
+/**
+ * Factory class for creating {@see FormDataBoundary} DTO instances from
+ * `content-type` headers.
+ *
+ * This class is responsible for parsing the boundary parameter from
+ * `content-type` headers in `multipart/xxx` requests.
+ *
+ * @internal this is an internal library class, please do not use it in your code
+ * @psalm-internal Boson\Component\Http\Body
+ */
 final class FormDataBoundaryFactory
 {
+    /**
+     * Attempts to create a {@see FormDataBoundary} instance from a
+     * `content-type` header string value
+     */
     public function tryCreateFromContentType(string $header): ?FormDataBoundary
     {
         // Lookup for "boundary=" prefix
@@ -29,13 +43,16 @@ final class FormDataBoundaryFactory
             $endsWith = \strpos($boundaryPayload, ';');
 
             // In case of boundary is last header segment: `boundary=value`
-            if ($endsWith === false) {
-                return new FormDataBoundary($boundaryPayload);
+            $boundaryPayload = \trim($endsWith === false
+                ? $boundaryPayload
+                : \substr($boundaryPayload, 0, $endsWith));
+
+            if ($boundaryPayload === '') {
+                return null;
             }
 
             // In case of boundary is last header segment: `boundary=value; other=42`
-            /** @phpstan-ignore-next-line payload value cannot be empty (checked above) */
-            return new FormDataBoundary(\substr($boundaryPayload, 0, $endsWith));
+            return new FormDataBoundary($boundaryPayload);
         }
 
         // Lookup for trailing `"`
@@ -46,7 +63,7 @@ final class FormDataBoundaryFactory
             return null;
         }
 
-        $boundaryPayload = \substr($boundaryPayload, 1, $endsWith - 1);
+        $boundaryPayload = \trim(\substr($boundaryPayload, 1, $endsWith - 1));
 
         if ($boundaryPayload === '') {
             return null;
