0e448a58b428600f27b144879563e15d196abd93
Kirill Nesmeyanov <nesk@xakep.ru>
2026-01-14 03:37:40 +0300

    Allow error pipe customization

--- libs/component/runtime/src/WebView/Api/Schemes/Handler/StderrErrorHandler.php
+++ libs/component/runtime/src/WebView/Api/Schemes/Handler/StderrErrorHandler.php
@@ -9,9 +9,23 @@ use Boson\WebView\WebView;
 
 final readonly class StderrErrorHandler implements ErrorHandlerInterface
 {
+    /**
+     * @var resource
+     */
+    public const mixed DEFAULT_ERROR_PIPE = \STDERR;
+
+    public function __construct(
+        /**
+         * @var resource
+         */
+        private mixed $pipe = self::DEFAULT_ERROR_PIPE,
+    ) {}
+
     public function handle(WebView $context, RequestInterface $request, \Throwable $exception): null
     {
-        \fwrite(\STDERR, $exception . "\n");
+        if (\is_resource($this->pipe)) {
+            \fwrite($this->pipe, $exception . "\n");
+        }
 
         return null;
     }
