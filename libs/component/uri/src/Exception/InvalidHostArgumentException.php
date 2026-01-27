17dac38abb19acfba8e86a2e30d45fe1f054f91e
Kirill Nesmeyanov <nesk@xakep.ru>
2026-01-27 03:39:53 +0300

    Fix uri linter issues

--- libs/component/uri/src/Exception/InvalidHostArgumentException.php
+++ libs/component/uri/src/Exception/InvalidHostArgumentException.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Boson\Component\Uri\Exception;
 
+/**
+ * @phpstan-consistent-constructor
+ */
 class InvalidHostArgumentException extends InvalidUriComponentArgumentException
 {
     public static function getComponentName(): string
--- libs/component/uri/src/Exception/InvalidUriComponentArgumentException.php
+++ libs/component/uri/src/Exception/InvalidUriComponentArgumentException.php
@@ -6,6 +6,9 @@ namespace Boson\Component\Uri\Exception;
 
 use Boson\Contracts\Uri\Exception\InvalidArgumentExceptionInterface;
 
+/**
+ * @phpstan-consistent-constructor
+ */
 abstract class InvalidUriComponentArgumentException extends UriComponentException implements
     InvalidArgumentExceptionInterface
 {
@@ -13,43 +16,47 @@ abstract class InvalidUriComponentArgumentException extends UriComponentExceptio
     final public const int ERROR_CODE_INVALID = 0x02;
     final public const int ERROR_CODE_EMPTY = 0x03;
 
-    final public function __construct(string $message, int $code = 0, ?\Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
     /**
      * @return non-empty-string
      */
     abstract public static function getComponentName(): string;
 
-    public static function becauseStringableErrorOccurs(\Throwable $e): static
+    final public static function becauseStringableErrorOccurs(\Throwable $e): static
     {
+        assert(self::class !== static::class, 'Invalid static call context');
+
         $message = \vsprintf('An error occurred while casting URI %s to string: %s', [
             static::getComponentName(),
             $e->getMessage(),
         ]);
 
+        /** @phpstan-ignore-next-line : The context is checked above in the assertion */
         return new static($message, self::ERROR_CODE_IN_STRINGABLE, $e);
     }
 
-    public static function becauseComponentMustBe(string $expected, mixed $given, ?\Throwable $prev = null): static
+    final public static function becauseComponentMustBe(string $expected, mixed $given, ?\Throwable $prev = null): static
     {
+        assert(self::class !== static::class, 'Invalid static call context');
+
         $message = \vsprintf('An URI %s must be %s, but %s given', [
             static::getComponentName(),
             $expected,
             self::getType($given),
         ]);
 
+        /** @phpstan-ignore-next-line : The context is checked above in the assertion */
         return new static($message, self::ERROR_CODE_INVALID, $prev);
     }
 
-    public static function becauseComponentIsEmpty(?\Throwable $prev = null): static
+    final public static function becauseComponentIsEmpty(?\Throwable $prev = null): static
     {
+        assert(self::class !== static::class, 'Invalid static call context');
+
         $message = \vsprintf('An URI %s cannot be empty', [
             static::getComponentName(),
         ]);
 
+        /** @phpstan-ignore-next-line : The context is checked above in the assertion */
         return new static($message, self::ERROR_CODE_EMPTY, $prev);
     }
 }
